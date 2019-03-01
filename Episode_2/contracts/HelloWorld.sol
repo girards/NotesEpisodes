d2c6d174270546b78b5ba482040a661260843a99
Nicolas Girardot <nicolas.girardot@neofacto.com>
2019-03-01 17:12:02 +0100

    File are now working for episode 2

--- Episode_2/contracts/HelloWorld.sol
+++ Episode_2/contracts/HelloWorld.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.18;
 
 contract HelloWorld {
 
-    function print() public pure returns (string) {
+    function print() public pure returns (string memory) {
         return ("Hello World !");
     }
 }
